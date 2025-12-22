a8f4e6211f3b7171e100cd73386bb67820fb82ce
Sanchit-Mangle <sanchit.mangle@devanta.tech>
2025-12-22 17:22:25 +0530

    folder strutur defined

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Home/Banner'
+import HeroSection from '../components/Home/HeroSection'
+
+const Home = () => {
+    return (
+        <div>
+            <Banner />
+            <HeroSection />
+        </div>
+    )
+}
+
+export default Home
